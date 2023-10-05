07cea384e555d151ac400e68d7356ae35c3603a9
ManarAbdelhalimMohamed <145897768+ManarAbdelhalimMohahmed@users.noreply.github.com>
2023-10-05 23:38:44 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints "Programming is like building a multilingual puzzle,
  * followed by a newline."
  * return 0.
@@ -9,6 +9,6 @@
 int main(void) 
 
 {
-puts("programming is like building a multilingual puzzle");
-return(0);	
-}	
+        puts("\"programming is like building a multilingual puzzle");
+	return(0);	
+	}	
