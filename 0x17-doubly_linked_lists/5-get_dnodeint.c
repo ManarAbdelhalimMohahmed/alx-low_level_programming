663c5460f934ac6c3bda8ceaaf77ed2036fa93aa
ManarAbdelhalimMohamed <145897768+ManarAbdelhalimMohahmed@users.noreply.github.com>
2024-01-12 07:51:39 +0200

    Add files via upload

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * calc_len_list - calculates length of a list
+ * @head: head
+ * Return: length
+*/
+unsigned int calc_len_list(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	unsigned int count = 0;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
+/**
+ * get_dnodeint_at_index - get node at specific index
+ * @head: head
+ * @index: index
+ * Return:specified node or null on failure
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int count = 0;
+
+	if (head == NULL || index >= calc_len_list(temp))
+		return (NULL);
+	while (count < index && temp->next != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (temp == NULL)
+		return (NULL);
+	return (temp);
+}
